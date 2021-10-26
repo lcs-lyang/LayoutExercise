c453e95b96f24fb77ee1bb25db611a5f4a027496
Lillian Yang <lyang@lcs.on.ca>
2021-10-26 14:44:48 -0400

    Created a view of my Grad day in Antigua. Added different font weights and titles.

--- LayoutExercise/Views/ContentView.swift
+++ LayoutExercise/Views/ContentView.swift
@@ -9,9 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ScrollView{
+            
+            VStack{
+                
+                Text("Graduation Day")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                
+                Text("Island Academy 2021")
+                    .font(.title)
+                    .foregroundColor(.gray)
+                    .fontWeight(.ultraLight)
+                
+                Image("grads")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("""
+            
+            Huih heduiwhdi whdiahkf yefgka wd hwdjbq hjdbqiwg hwudfgqiu wufigbwuie qhwuidgqiu vxyquvduyqw uiqegduiqg fhweuifbiw ieuwgdfuiqw qiwudgqbwd iqudiuqhwd.
+                     
+            Hiiu shdbhjabd iqwegdbd uwdhqkwd iwefbdqbd qiwudqw dsbhqbdw qwiduhqwdb aiuwgdw wbdhwdbuqiw uqgwdw lsfhwjedq jahsdbhja qwjhdbhjq qiwdbquyw dnjkqd qhbdu qiuw.
+            """)
+                
+                Spacer()
+                
+            }
+        }
+        
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
