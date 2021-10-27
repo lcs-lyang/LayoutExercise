27d92c924ee978c780bb621921c9a6cfa77571b4
Lillian Yang <lyang@lcs.on.ca>
2021-10-27 19:44:03 -0400

    Created my second view. Added A VStack which has a Hstack. Within the Hstack there are 2 more Vstacks.

---
+++ LayoutExercise/Views/ExerciseTwo.swift
@@ -0,0 +1,70 @@
+//
+//  ExerciseTwo.swift
+//  LayoutExercise
+//
+//  Created by Lillian Yang on 2021-10-26.
+//
+
+import SwiftUI
+
+struct ExerciseTwo: View {
+    var body: some View {
+        
+        ScrollView{
+            
+            VStack{
+                
+                Text("Title")
+                    .font(Font.custom("Arial Bold", size: 72))
+                
+                Image ("beauty")
+                    .resizable()
+                    .scaledToFit()
+                
+                
+                Spacer()
+                
+            }
+            
+            HStack{
+                
+                VStack{
+                    
+                    Text("Nevada")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                    
+                }
+              
+                VStack{
+                    
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.")
+                    
+                    
+                    
+                    
+                    
+                }
+                    
+                    
+                }
+            
+        }
+        
+        
+        }
+        
+        
+       
+        
+        
+    }
+
+
+struct ExerciseTwo_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseTwo()
+    }
+}
