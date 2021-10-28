3d1120a9c662e5eae75fa49088256b0a372d0098
Lillian Yang <lyang@lcs.on.ca>
2021-10-27 20:13:21 -0400

    Created ExerciseThree.

---
+++ LayoutExercise/Views/ExerciseThree.swift
@@ -0,0 +1,20 @@
+//
+//  ExerciseThree.swift
+//  LayoutExercise
+//
+//  Created by Lillian Yang on 2021-10-27.
+//
+
+import SwiftUI
+
+struct ExerciseThree: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExerciseThree_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseThree()
+    }
+}
