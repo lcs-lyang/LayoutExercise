f5f43555eb7c1d234ef67c574f1040dae59e6de7
Lillian Yang <lyang@lcs.on.ca>
2021-10-28 20:09:10 -0400

    Finished ExerciseThree.

--- LayoutExercise/Views/ExerciseThree.swift
+++ LayoutExercise/Views/ExerciseThree.swift
@@ -9,7 +9,67 @@ import SwiftUI
 
 struct ExerciseThree: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack{
+            
+            VStack{
+                
+                Text("Brita")
+                    .fontWeight(.bold)
+                    .font(Font.custom("Gill Sans", size: 30))
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.")
+                
+                HStack{
+                    
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.")
+                    
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.")
+                }
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam..")
+                Spacer()
+                
+            }
+            
+            VStack{
+                    
+                    Image("cattt")
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack{
+                    
+                    Image("flores")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Image("flores")
+                        .resizable()
+                        .scaledToFit()
+                    
+                }
+                
+                Image("flores")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("cattt")
+                .resizable()
+                .scaledToFit()
+                
+                Image("cattt")
+                .resizable()
+                .scaledToFit()
+                
+                Spacer()
+                
+                
+                
+            }
+            
+        }
+        
+        
     }
 }
 
